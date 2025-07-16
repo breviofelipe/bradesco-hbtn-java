f88b6954b02412481e4542a39106778d2ebe3cf1
Felipe Brevio <felipebrevio05@gmail.com>
2025-07-16 20:17:54 -0300

    feat personagem

---
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -0,0 +1,5 @@
+package Classes_Objetos.personagem;
+public class PersonagemGame {
+    public String nome;
+    public int saudeAtual;
+}
---
+++ Classes_Objetos/personagem/Program.java
@@ -0,0 +1,15 @@
+package Classes_Objetos.personagem;
+public class Program {
+    public static void main(String[] args) {
+        PersonagemGame personagem1 = new PersonagemGame();
+        PersonagemGame personagem2 = new PersonagemGame();
+
+        personagem1.nome = "Sonic";
+        personagem1.saudeAtual= 10;
+
+        personagem2.nome = "Mario";
+        personagem2.saudeAtual = 100;
+        System.out.println("Nome: " + personagem1.nome + " - Saúde: " + personagem1.saudeAtual);
+        System.out.println("Nome: " + personagem2.nome + " - Saúde: " + personagem2.saudeAtual);
+    }
+}
