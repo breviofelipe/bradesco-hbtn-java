4b5ab70226173b1c5ff0da750af5c15391d41c34
Brevio <felipebrevio05@gmail.com>
2025-07-17 11:55:26 +0000

    fix

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,19 @@
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setSaudeAtual(int saudeAtual){
+        this.saudeAtual = saudeAtual;
+    }
+
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+}
