dfa8284d323e9cdd640443a423a427828f0bc8ce
Brevio <felipebervio05@gmail.com>
2025-08-19 17:12:56 +0000

    livro fizx

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,6 +1,9 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
 public class LivroEdicaoOuro extends Livro{
 
-    public LivroEdicaoOuro(String string, String string2, double d) {
+    public LivroEdicaoOuro(String string, String string2, double d) throws AutorInvalidoException, LivroInvalidoException {
         super(string, string2, d);
     }
     
