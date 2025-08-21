5f489c40643c403ac06b06e0aa5d5c452c26a33c
Brevio <felipebervio05@gmail.com>
2025-08-21 12:27:54 +0000

    feat contas especias

---
+++ 0x03/contas_especiais/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception{
+    private String mensagem;
+
+    public OperacaoInvalidaException (String mensagem){
+        super(mensagem);
+        this.mensagem = mensagem;
+    }
+
+    public String getMessage(){
+        return mensagem;
+    }
+}
