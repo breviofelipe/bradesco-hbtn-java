5df12f3c0b29b11bc879834da11f5ae6425070c9
Felipe Brevio <felipebrevio05@gmail.com>
2025-09-15 20:08:30 -0300

    feat todo

---
+++ List/todo/Tarefa.java
@@ -0,0 +1,37 @@
+public class Tarefa {
+    private String descricao;
+    private boolean estahFeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador) {
+        if (descricao == null || descricao.trim().isEmpty()) {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+        this.descricao = descricao;
+        this.identificador = identificador;
+        this.estahFeita = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void modificarDescricao(String novaDescricao) {
+        if (novaDescricao == null || novaDescricao.trim().isEmpty()) {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+        this.descricao = novaDescricao;
+    }
+}
