844cfca563fc8bc5e9ae1e19b750e1471f9a53db
Felipe Brevio <felipebrevio05@gmail.com>
2025-10-06 20:29:30 -0300

    thread 0

---
+++ threads/0/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException {
+        Contador contador = new Contador();
+        ThreadContador thread1 = new ThreadContador(contador);
+        thread1.start();
+        ThreadContador thread2 = new ThreadContador(contador);
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("Valor final do contador: " + contador.getContagem());
+    }
+}
